74c2f739b78673a668a330eb5f875cfd6d356f89
Codegod54 <130825098+Codegod54@users.noreply.github.com>
2023-09-25 09:26:07 +0545

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#Calculates 6 digit addition, multiplication and divide.
+num1 =int(input("enter digit: "))
+num2 =int(input("enter digit: "))
+num3 =int(input("enter digit: "))
+num4 =int(input("enter digit: "))
+num5 =int(input("enter digit: "))
+num6 =int(input("enter digit: "))
+#Addition 
+sum =  num1+num2+num3+num4+num5+num6
+print('Sum is ', sum )
+#Multiplication
+mux = num1*num2*num3*num4*num5*num6
+print('Multiple is ', mux)
+#Divide
+div = sum/mux
+print('Division is ', div)
